aabc983d4074ebdc7fb40805d51e8d54b1e60703
shrutix <shrspam8@gmail.com>
2019-11-20 18:36:58 -0500

    first cuda exercises

---
+++ 01-AcceleratingApplicationsCUDA/02-multi-block-loop.cu
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+/*
+ * Refactor `loop` to be a CUDA Kernel. The new kernel should
+ * only do the work of 1 iteration of the original loop.
+ */
+
+__global__ void loop()
+{
+  /* for (int i = 0; i < N; ++i)
+  {
+    printf("This is iteration number %d\n", i);
+  } */
+  int i = blockIdx.x * blockDim.x + threadIdx.x;
+  printf("%d\n", i);
+}
+
+int main()
+{
+  /*
+   * When refactoring `loop` to launch as a kernel, be sure
+   * to use the execution configuration to control how many
+   * "iterations" to perform.
+   *
+   * For this exercise, be sure to use more than 1 block in
+   * the execution configuration.
+   */
+
+  /* int N = 10;
+  loop(N); */
+  
+  loop<<<2, 5>>>();
+  cudaDeviceSynchronize();
+}
